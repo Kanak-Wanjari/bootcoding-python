4d7e98691aea1dc5f80ad9128eb004d341eabd3f
Kanak Wanjari <wanjarikanak@gmail.com>
2024-01-03 17:41:15 +0530

    Commit

---
+++ Python Assignment Day 1/Power_of_2.py
@@ -0,0 +1,3 @@
+for i in range(6):
+    results = 2**i
+print(f"2 raised to the power of {i} is: {results}")
---
+++ Python Assignment Day 1/Sum_of_Even_No.py
@@ -0,0 +1,5 @@
+sum_of_even = 0
+for num in range(1,16):
+    if num%2==0:
+        sum_of_even +=num
+print("Sum of even numbers from 1 to 15 is :",sum_of_even)
