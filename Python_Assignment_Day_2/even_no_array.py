9bf19ac29f1150a94114506c3288f00b750e5cac
Kanak Wanjari <wanjarikanak@gmail.com>
2024-01-08 12:01:53 +0530

    Commit

--- Python_Assignment_Day_2/even_no_array.py
+++ Python_Assignment_Day_2/even_no_array.py
@@ -1,5 +1,5 @@
-def print_even_numbers(arr):
-    for num in arr:
+def print_even_numbers(array):
+    for num in array:
         if num % 2 == 0:
             print(num)
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
