533ac91ede4954e3cdd7e865ea5c2d07f9c05dc8
Kanak Wanjari <wanjarikanak@gmail.com>
2024-01-09 12:55:26 +0530

    Commit

--- Python_Assignment_Day_5/insert_item.py
+++ Python_Assignment_Day_5/insert_item.py
@@ -1,15 +1,15 @@
-'''import array
+import array
 original_array = array.array('i', [1, 3, 5, 7, 9])
 print("Original array:", original_array)
 new_value = int(input("Enter the value to insert: "))
 original_array.insert(1, new_value)
-print("New array:", original_array)'''
+print("New array:", original_array)
 
-original_list = [1, 3, 5, 7, 9]
+'''original_list = [1, 3, 5, 7, 9]
 print("Original list:", original_list)
 
 new_value = int(input("Enter the value to insert: "))
 
 original_list.insert(1, new_value)
 
-print("New list:", original_list)
+print("New list:", original_list)'''
