7ad2f8b9a500e2228b659265ed647897df42b113
Kanak Wanjari <wanjarikanak@gmail.com>
2024-02-01 11:52:22 +0530

    commit

---
+++ Python_Assignment_Day_29/shapes_hierarchy.py
@@ -0,0 +1,30 @@
+# Define pi as a constant
+pi = 3.14159
+
+class Shape:
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius**2
+
+    def perimeter(self):
+        return 2 * pi * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
