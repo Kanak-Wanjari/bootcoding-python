a942791140afb6e2d3f9671e53965f52fe68e1dc
Kanak Wanjari <wanjarikanak@gmail.com>
2024-02-06 11:54:37 +0530

    Commit

--- Python_Assignment_Day_36/score_less.py
+++ Python_Assignment_Day_36/score_less.py
@@ -3,10 +3,10 @@ import csv
 file_path='C:\Python Bootcoding\Bootcoding_Internship_Online_Test.csv'
 
 with open(file_path,'r') as csv_file:
-    csv_reader=csv.DictReader(csv_file,delimiter='\t')
+    csv_reader=csv.DictReader(csv_file)
     data = list(csv_reader)
 
 low_scorers=[entry for entry in data if float(entry['Score'])<=0]
 
 for person in low_scorers:
-    print(f"Email:{person['Email']}, Name: {person['Name']}, Score: {person['Score']}")
+    print(f"Name: {person['Name']}, Score: {person['Score']}")
