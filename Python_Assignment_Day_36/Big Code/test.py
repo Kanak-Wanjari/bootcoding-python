ed40d15508434adfbc6a0b3940aa29ff02c161f9
Kanak Wanjari <wanjarikanak@gmail.com>
2024-02-06 17:51:33 +0530

    Commit

--- Python_Assignment_Day_36/Big Code/test.py
+++ Python_Assignment_Day_36/Big Code/test.py
@@ -7,18 +7,17 @@ output_directory = r'C:\Python Bootcoding\bootcoding-python\Python_Assignment_Da
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
-with open(input_file_path, 'r') as file:
+with open(input_file_path,'r') as file:
     csv_reader = csv.DictReader(file)
     data = list(csv_reader)
 
-unique_zip_values = set(row['ZIP'] for row in data if 'ZIP' in row)
+unique_zip_values = set(row['ZIP'] for row in data if 'ZIP')
 
 for zip_value in unique_zip_values:
     filtered_data = [row for row in data if row.get('ZIP') == zip_value]
     output_file_path = os.path.join(output_directory, f'{zip_value}_data.csv')
-    with open(output_file_path, 'w', newline='') as output_file:
+    with open(output_file_path,'w',newline='') as output_file:
         fieldnames = data[0].keys()
         csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
-
         csv_writer.writeheader()
         csv_writer.writerows(filtered_data)
