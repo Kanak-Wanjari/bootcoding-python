1a09641e0d2183a82a448e9c73fc4573ff7d7bad
Kanak Wanjari <wanjarikanak@gmail.com>
2024-02-06 12:28:07 +0530

    Commit

---
+++ Python_Assignment_Day_36/attempt_questions.py
@@ -0,0 +1,13 @@
+import csv
+
+file_path='C:\Python Bootcoding\Bootcoding_Internship_Online_Test.csv'
+
+with open(file_path,'r') as csv_file:
+    csv_reader=csv.DictReader(csv_file)
+    data=list(csv_reader)
+
+target_accuracy = input("Enter the target percentage (eg: 10.00%): ")
+
+target_accuracy_count = sum(1 for entry in data if 'Accuracy' in entry and entry['Accuracy']==target_accuracy)
+
+print(f"Total Count of individuals with {target_accuracy} accuracy:{target_accuracy_count}")
