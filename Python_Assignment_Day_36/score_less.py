1b1f641f7fe814ca9180a65476e789ac520bf318
Kanak Wanjari <wanjarikanak@gmail.com>
2024-02-06 11:45:58 +0530

    Commit

---
+++ Python_Assignment_Day_36/score_less.py
@@ -0,0 +1,12 @@
+import csv
+
+file_path='C:\Python Bootcoding\Bootcoding_Internship_Online_Test.csv'
+
+with open(file_path,'r') as csv_file:
+    csv_reader=csv.DictReader(csv_file,delimiter='\t')
+    data = list(csv_reader)
+
+low_scorers=[entry for entry in data if float(entry['Score'])<=0]
+
+for person in low_scorers:
+    print(f"Email:{person['Email']}, Name: {person['Name']}, Score: {person['Score']}")
